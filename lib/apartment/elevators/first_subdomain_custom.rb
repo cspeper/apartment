27132141ea0590bdd10768f3e8c1d7d0a392332c
Scott Peper <cspeper@gmail.com>
2014-01-31 14:46:38 -0500

    Add FirstSubdominaCustom elevator

--- lib/apartment/elevators/first_subdomain_custom.rb
+++ lib/apartment/elevators/first_subdomain_custom.rb
@@ -2,11 +2,7 @@ require 'apartment/elevators/first_subdomain'
 
 module Apartment
   module Elevators
-    # Provides a rack based tenant switching solution based on the first subdomain
-    # of a given domain name.
-    # eg:
-    #     - example1.domain.com               => example1
-    #     - example2.something.domain.com     => example2
+
     class FirstSubdomainCustom < FirstSubdomain
 
       def initialize(app, processor)
