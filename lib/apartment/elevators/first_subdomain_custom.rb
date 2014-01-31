8d97975780618f6fd4647e5e91a446bf2fcd6798
Scott Peper <cspeper@gmail.com>
2014-01-31 14:42:15 -0500

    Add FirstSubdominaCustom elevator

--- lib/apartment/elevators/first_subdomain_custom.rb
+++ lib/apartment/elevators/first_subdomain_custom.rb
@@ -9,9 +9,14 @@ module Apartment
     #     - example2.something.domain.com     => example2
     class FirstSubdomainCustom < FirstSubdomain
 
-      def parse_tenant_name(request)
-        subdomain = super
-        subdomain == 'www' ? 'open' : subdomain
+      def initialize(app, processor)
+        @app = app
+        @subdomain_mapper = processor
+        @processor = method(:parse_tenant_name)
+      end
+
+      def parse_tenant_name(subdomain)
+        @subdomain_mapper.call(subdomain)
       end
     end
   end
